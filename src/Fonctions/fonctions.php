24203678f296d6716f1cc1c93011511344e38948
mikabgn <mika.bougnon@yahoo.com>
2024-11-13 15:19:02 +0100

    Fonction calcul complexite Mdp

--- src/Fonctions/fonctions.php
+++ src/Fonctions/fonctions.php
@@ -11,7 +11,15 @@ function GenereMDP($nbChar) :string{
     return "secret";
 }
 
-function CalculComplexiteMdp($mdp) :int{
+    function CalculComplexiteMdp($mdp) :int {
+        $L = strlen($mdp); // Longueur du mot de passe
 
+        $B = 0;
+        if (preg_match('/[a-z]/', $mdp)) $B += 26; // Lettres minuscules
+        if (preg_match('/[A-Z]/', $mdp)) $B += 26; // Lettres majuscules
+        if (preg_match('/[0-9]/', $mdp)) $B += 10; // Chiffres
+        if (preg_match('/[^a-zA-Z0-9]/', $mdp)) $B += 10; // Caractères spéciaux
 
-}
+        $E = $L * log($B, 2); // Entropie en bits
+        return ($E) ;
+    }
