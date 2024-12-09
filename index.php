d00a3a09a974db97d8e0e1f30d6507df024d895c
mikabgn <mika.bougnon@yahoo.com>
2024-12-09 16:02:57 +0100

    Q4 et Q5

--- index.php
+++ index.php
@@ -47,9 +47,6 @@ else
     $action = "Action_Par_Defaut";
 //error_log("action : " . $action);
 //utiliser en débuggage pour avoir le type de connexion
-$Vue->addToCorps(new Vue_AfficherMessage("<br>Action $action<br>"));
-$Vue->addToCorps(new Vue_AfficherMessage("<br>Action $typeConnexion<br>"));
-
 
 switch ($typeConnexion) {
     case "visiteur" :
